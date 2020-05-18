b013945baf395876c29a6a1dc68fbe81f0c9674f
VladimirImir <VladimirImir>
2020-05-18 21:20:03 +0300

    Structures

---
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Point
+{
+	double x;
+	double y;
+};
+
+void main()
+{
+	int a;   // Объявление переменной 'A', типа 'int'.
+	Point A; // Объявление переменной 'A', типа 'Point'.
+	         // Создание экземпляра 'A', структуры 'Point'.
+
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+}
