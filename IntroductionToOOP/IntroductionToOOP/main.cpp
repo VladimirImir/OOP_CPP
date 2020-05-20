75af2f43c152c24ad430aa55da0dcf8c1b0055d2
VladimirImir <VladimirImir>
2020-05-20 21:43:02 +0300

    master1

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 using namespace std;
 
-struct Point
+class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return  x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+class Point3D :public Point
+{
+	double z;
+public:
+	double get_z()const
+	{
+		return z;
+	}
+	void set_z()
+	{
+		this->z = z;
+	}
 };
 
 void main()
@@ -13,10 +44,14 @@ void main()
 	Point A; // Объявление переменной 'A', типа 'Point'.
 	         // Создание экземпляра 'A', структуры 'Point'.
 
-	A.x = 2;
-	A.y = 3;
-	cout << A.x << "\t" << A.y << endl;
+
+
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 
 	Point* pA = &A;
-	cout << pA->x << "\t" << pA->y << endl;
+	cout << pA->get_x() << "\t" << pA->get_y() << endl;
+
+	Point3D B;
 }
